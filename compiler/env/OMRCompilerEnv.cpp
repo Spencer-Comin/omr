8e2db8527e3831c574d9efda6b78ff8a609c47d7
Spencer Comin <spencer.comin@ibm.com>
2025-04-22 16:57:56 -0400

    Use contained portlib
    REWORD ME

--- compiler/env/OMRCompilerEnv.cpp
+++ compiler/env/OMRCompilerEnv.cpp
@@ -76,7 +76,7 @@ OMR::CompilerEnv::initializeHostEnvironment()
 
    // Initialize the host CPU by querying the host processor
    //
-   host.cpu = TR::CPU::detect(TR::Compiler->omrPortLib);
+   host.cpu = TR::CPU::detect(omrPortLib);
 
    // Host major operating system
    //
@@ -123,7 +123,7 @@ OMR::CompilerEnv::initializeTargetEnvironment()
 
    // Initialize the target CPU by querying the host processor
    //
-   target.cpu = TR::CPU::detect(TR::Compiler->omrPortLib);
+   target.cpu = TR::CPU::detect(omrPortLib);
    TR::CPU::initializeTargetProcessorInfo();
 
    // Target major operating system
@@ -165,7 +165,7 @@ OMR::CompilerEnv::initializeRelocatableTargetEnvironment()
 
    // Initialize the relocatable target CPU by querying the host processor
    //
-   relocatableTarget.cpu = TR::CPU::detect(TR::Compiler->omrPortLib);
+   relocatableTarget.cpu = TR::CPU::detect(omrPortLib);
 
    // Target major operating system
    //
